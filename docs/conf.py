cbf3892443d71a0c93477b6cc7f80e1cdc08d710
Gawaine1 <3057801042@qq.com>
2026-05-11 12:15:13 +0800

    docs: add Sphinx and ReadTheDocs configuration

---
+++ docs/conf.py
@@ -0,0 +1,19 @@
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+project = 'Your Project Name'
+copyright = '2026, Your Name'
+author = 'Your Name'
+
+# -- General configuration ---------------------------------------------------
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
